9bea39f2703dc8547c9be62fca3b5d337130c195
yubao0235 <yubao0235@163.com>
2026-08-10 15:56:19 +0800

    Update hotel_gather.py

--- md/hotel_gather.py
+++ md/hotel_gather.py
@@ -8,7 +8,6 @@ import sys
 # --- 配置区 ---
 SOURCES = [
     "https://oer.us.ci/get/jilin.m3u", 
-    "https://222.boyu.ccwu.cc/get/001.m3u",
     "https://yuxx.de5.net/edcab778.m3u",	
     "https://yuxx.de5.net/33fc12a0.m3u",	
     "https://yuxx.de5.net/3c1f32b7.m3u",	
